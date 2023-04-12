0310ef5371e9b9f8606ac70e8effc4913ef7cf46
jeremy <woghd1008@gmail.com>
2023-04-12 18:16:41 +0900

    Ch02_01 - 05

---
+++ Java_B/src/com/java/Ex2/Ex2_2.java
@@ -0,0 +1,12 @@
+package com.java.Ex2;
+
+public class Ex2_2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world");
+		System.out.println("3+5=");
+		System.out.println(3+5);
+
+	}
+
+}
---
+++ Java_B/src/com/java/Ex2/Ex2_5.java
@@ -0,0 +1,16 @@
+package com.java.Ex2;
+
+public class Ex2_5 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 5;
+		
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+
+	}
+
+}
